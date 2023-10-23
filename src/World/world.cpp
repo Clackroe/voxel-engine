96ead19393862429f125b8d2558d72c06c7adaf4
Xander Cole <acole67@uncc.edu>
2023-10-23 19:39:03 -0400

    Cleanup comments

--- src/World/world.cpp
+++ src/World/world.cpp
@@ -3,64 +3,6 @@
 
 
 
-unsigned int compileShaders(const char* vertex, const char* frag) {
-
-    std::string vShadSrc = FileUtils::read_file(vertex);
-    std::string fShadSrc = FileUtils::read_file(frag);
-
-    const char* vertShad = vShadSrc.c_str();
-    const char* fragShad = fShadSrc.c_str();
-
-    int result;
-    char infoLog[512];
-
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertShad, NULL);
-    glCompileShader(vertexShader);
-
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
-
-    if (!result) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERRO WITH VERT SHADER" << std::endl;
-    }
-
-    unsigned int fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragShader, 1, &fragShad, NULL);
-    glCompileShader(fragShader);
-
-    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
-
-    if (!result) {
-        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
-        std::cout << "ERROR WITH FRAG SHADER COMPILE" << std::endl;
-    }
-
-    //Link Shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragShader);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
-
-    if (!result) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR LINKING PROGRAM" << std::endl;
-
-    }
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragShader);
-
-    return shaderProgram;
-
-
-
-}
-
-
-
 
 World::World(Player* plyr)
 {
@@ -71,15 +13,13 @@ World::World(Player* plyr)
 
 
     //projections
-
-       // std::cout << "FIRED" << std::endl;
     float vertices[] = {
          0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right
          0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f// bottom right
         - 0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,// bottom left
         -0.5f,  0.5f, 0.0f,   0.5f, 0.5f, 0.5f// top left 
     };
-    unsigned int indices[] = {  // note that we start from 0!
+    unsigned int indices[] = {
         0, 1, 2,
         2, 0, 3
     };
@@ -117,26 +57,15 @@ World::~World() {
 
 void World::render(float deltaTime) {
 
-    // std::cout << "Render" << std::endl;
-
     glm::mat4 trans = glm::mat4(1.0f);
     trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
     trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-
-    // unsigned int transformLoc = glGetUniformLocation(SHADER_PROGRAM->ID, "transform");
-    // glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
-    // glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)1920 / (float)1080, 0.1f, 100.0f);
 
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
     model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
-
-    // glm::mat4 view = glm::mat4(1.0f);
-    // // note that we're translating the scene in the reverse direction of where we want to move
-    // view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     glm::mat4 view = player->cam->getViewMatrix();
 
     glm::mat4 projection;
@@ -153,59 +82,12 @@ void World::render(float deltaTime) {
     glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
     SHADER_PROGRAM->use();
-    // float time = glfwGetTime();
-    //
-    // float greenValue = (sin(time) / 2.0f) + 0.5f;
-    // int vertexColorLocation = glGetUniformLocation(SHADER_PROGRAM, "color");
-
-    // glUniform4f(vertexColorLocation, 1.0, greenValue, 1.0, 1.0);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    // glDrawArrays(GL_TRIANGLES, 0, 36);
-    // std::cout << "PostDraw" << std::endl;
-
 }
 
 void World::update(float deltaTime) {
 
     player->processInput(deltaTime);
-    // if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    //     cam->processKeyboardInput(FORWARD, 1.0);
-    // if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    //     cam->processKeyboardInput(BACKWARD, 1.0);
-    // if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    //     cam->processKeyboardInput(LEFT, 1.0);
-    // if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    //     cam->processKeyboardInput(RIGHT, 1.0);
 
 
 }
-
-// // glfw: whenever the mouse moves, this callback is called
-// // -------------------------------------------------------
-// void mouse_callback(GLFWwindow* window, double xposIn, double yposIn, Camera* cam)
-// {
-    // float xpos = static_cast<float>(xposIn);
-//     float ypos = static_cast<float>(yposIn);
-
-//     if (firstMouse)
-//     {
-//         lastX = xpos;
-//         lastY = ypos;
-//         firstMouse = false;
-//     }
-
-//     float xoffset = xpos - lastX;
-//     float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-//     lastX = xpos;
-//     lastY = ypos;
-
-//     cam->processMouseInput(xoffset, yoffset);
-// }
-
-// // glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// // ----------------------------------------------------------------------
-// void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-// {
-//     camera.ProcessMouseScroll(static_cast<float>(yoffset));
-// }
